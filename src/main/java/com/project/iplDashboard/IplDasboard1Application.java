146fd02aecfcb18938adcf31256a2208c45df6df
arpit-15s <arpitsrivastav15s@gmail.com>
2023-03-30 10:57:01 +0530

    Added springbatch preprocessor

---
+++ src/main/java/com/project/iplDashboard/IplDasboard1Application.java
@@ -0,0 +1,13 @@
+package com.project.iplDashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplDasboard1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IplDasboard1Application.class, args);
+	}
+
+}
